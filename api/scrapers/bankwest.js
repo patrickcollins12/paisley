865e44e94ce2e2c804cf3d99fb534c81206c5293
patrick <pcollins1@gmail.com>
2023-12-26 21:10:04 +1100

    adding playwright and the first couple scrapers

---
+++ api/scrapers/bankwest.js
@@ -0,0 +1,28 @@
+import { test } from '@playwright/test';
+const path = require('path');
+
+const ConfigManager = require('../ConfigManager.js');
+const configManager = new ConfigManager('pfm');
+const config = configManager.readConfig();
+const bank_config = config['BankwestScraper'];
+
+test('test', async ({ page }) => {
+  await page.goto('https://ibs.bankwest.com.au/Session/PersonalLogin');
+  await page.getByRole('textbox', { name: 'Personal Access Number (PAN)' }).fill(bank_config['pan']);
+  await page.getByRole('textbox', { name: 'Password' }).fill(bank_config['password']);
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('button', { name: 'Accounts' }).click();
+  await page.getByRole('link', { name: 'Transaction search' }).click();
+  await page.locator('[id="_ctl0_ContentMain_ddlAccount"]').selectOption('[All]');
+  await page.locator('[id="_ctl0_ContentMain_ddlRangeOptions"]').selectOption('L30Days');
+  const downloadPromise = page.waitForEvent('download');
+  await page.getByRole('link', { name: 'Export' }).click();
+  const download = await downloadPromise;
+
+  // Wait for the download process to complete and save the downloaded file somewhere.
+  const fileName = [bank_config['csv_prepend'], process.pid, download.suggestedFilename()].join('_')
+  let csv_location = path.join( config['csv_watch'], fileName );
+  await download.saveAs(csv_location);
+  console.log(`Saved to ${csv_location}`)
+
+});
---
+++ api/scrapers/westpac.js
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+const path = require('path');
+
+const ConfigManager = require('../ConfigManager.js');
+const configManager = new ConfigManager('pfm');
+const config = configManager.readConfig();
+const bank_config = config['WestpacScraper'];
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.westpac.com.au/');
+  await page.getByRole('link', { name: 'Sign in' }).click();
+  await page.getByLabel('Customer ID', { exact: true }).click();
+  await page.getByLabel('Customer ID', { exact: true }).fill(bank_config['Customer ID']);
+  await page.getByLabel('Customer ID', { exact: true }).press('Tab');
+  await page.getByLabel('Password').fill(bank_config['Password']);
+  await page.locator('label').filter({ hasText: 'Remember customer ID Not recommended on public or shared devices' }).locator('span').nth(1).click();
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await page.goto('https://banking.westpac.com.au/secure/banking/reportsandexports/home');
+  await page.getByRole('link', { name: 'Export Transactions' }).click();
+  await page.getByRole('link', { name: 'a preset range' }).click();
+  await page.getByRole('link', { name: 'Last 60 days' }).click();
+//   await page.getByPlaceholder(' Select accounts').click();
+//   await page.getByRole('link', { name: 'Select dropdown' }).click();
+//   await page.getByRole('link', { name: 'All', exact: true }).click();
+  const downloadPromise = page.waitForEvent('download');
+  await page.getByRole('button', { name: 'Export' }).click();
+  const download = await downloadPromise;
+
+  const fileName = [bank_config['csv_prepend'], process.pid, download.suggestedFilename()].join('_')
+  let csv_location = path.join( config['csv_watch'], fileName );
+  await download.saveAs(csv_location);
+  console.log(`Saved to ${csv_location}`)
+
+//   await download.saveAs('/tmp/westpac' + download.suggestedFilename());
+
+});
+
