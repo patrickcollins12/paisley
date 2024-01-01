a3cbd85c3819d432d43f4025a76cd6ff03147dfa
patrick <pcollins1@gmail.com>
2024-01-02 00:29:29 +1100

    cleanup

--- api/scrapers/swyftx.js
+++ api/scrapers/swyftx.js
@@ -29,15 +29,12 @@ async function getBalance(accessToken) {
                 "Authorization": `Bearer ${accessToken}`
             }
         });
+
         // [
         //     { assetId: 1, availableBalance: '0', stakingBalance: '0' },
         //     { assetId: 2, availableBalance: '0', stakingBalance: '0' },
-        //     {
-        //       assetId: 3,
-        //       availableBalance: '0.019820594506406805',
-        //       stakingBalance: '0'
-        //     }
-        //   ]
+        //     { assetId: 3, availableBalance: '0.01982', stakingBalance: '0' }
+        //  ]
         return response.data;
     } catch (error) {
         console.log(error);
@@ -49,16 +46,11 @@ async function getLiveRates() {
         const response = await axios.get("https://api.swyftx.com.au/markets/info/basic/");
         return response.data.reduce((acc, r) => ({ ...acc, [r["id"]]: r }), {});
         // '3': {
-        //     name: 'Bitcoin',
-        //     altName: 'Bitcoin',
-        //     code: 'BTC',
         //     id: 3,
-        //     rank: 1,
-        //     buy: '62807.70675005',
+        //     code: 'BTC',
+        //     name: 'Bitcoin',
+        //     ...
         //     sell: '62200.78267905',
-        //     spread: '0.97',
-        //     volume24H: 13880352943,
-        //     marketCap: 566141994744
         //   },
 
     } catch (error) {
@@ -66,7 +58,7 @@ async function getLiveRates() {
     }
 }
 
-async function doit() {
+async function apiCallsToSwyftx() {
     let balances = await getBalance(await getAccessToken())
     let rates = await getLiveRates()
 
@@ -87,7 +79,6 @@ async function doit() {
                     "balance": myBalance.toFixed(2),
                     "type": "BAL"
                 }
-                console.log(entry)
                 output.push( entry )
             }
 
@@ -99,14 +90,15 @@ async function doit() {
 }
 
 test('test', async ({ page }) => {
-    // await page.goto('https://www.swyftx.com.au/');
 
-    let data = await doit()
+    let data = await apiCallsToSwyftx()
 
     // setup the csv filename
     const dated = DateTime.now().setZone("Australia/Sydney").toISODate();
     let fn = `${bank_config['identifier']}_balance_${dated}.csv`
     let outCSVFile = path.join(config['csv_watch'], fn);
+    
+    // console.log(data)
 
     await util.saveDataToCSV(outCSVFile, data);
 
