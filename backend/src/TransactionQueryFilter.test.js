9331716667cdf97cde058660dcd3100e8e25201a
patrick <pcollins1@gmail.com>
2024-06-08 00:29:54 +1000

    got transaction query filter test running!

--- backend/src/TransactionQueryFilter.test.js
+++ backend/src/TransactionQueryFilter.test.js
@@ -1,32 +1,52 @@
-
-const TransactionQueryFilter = require('./TransactionQueryFilter.cjs');
+const axios = require('axios');
+const ExpressServer = require('./ExpressServer');
+const config = require('./Config');
+config.load();
+const database_setup = require('../test/BankDatabaseDummy.js');
+// const TransactionQueryFilter = require('./TransactionQueryFilter.cjs');
+const port = 4005
 
 describe('Test TransactionQuery', () => {
+    let expressServer;
+    let db;
 
-    function clean(str){
-        var regex = new RegExp("[\n\s]+", "g");
-        return str.replace(regex,' ').trim()
-      }
+    beforeAll(async () => {
+
+        db = database_setup()
+
+        // Start the Express Server
+        expressServer = new ExpressServer({
+            enableApiDocs: false,
+            port: port,
+            globalDisableAuth: true
+        });
+        await expressServer.start();
+        console.log('Server started for test.');
+    });
+
+    afterAll(async () => {
+        await expressServer.stop();
+    });
+
+    test('test /transactions', async () => {
+        const response = await axios.get(`http://localhost:${port}/api/transactions/`);
+        // console.log(response.data);
+        expect(response.data?.results.length).toBe(3)
+        expect(response.data?.resultSummary?.count).toBe(3)
+    });
+
+    test('test /transactions v2', async () => {
+        const url = `http://localhost:${port}/api/transactions/?filter[amount][<]=0`
+        // const url = `http://localhost:${port}/api/transactions/?${encodeURIComponent("filter[amount][<]")}=0`
+        const response = await axios.get(url);
+        expect(response.data?.resultSummary?.count).toBe(2)
+    });
+
+    test('test /transactions v3', async () => {
+        const url = `http://localhost:${port}/api/transactions/?filter[amount][>]=0`
+        // const url = `http://localhost:${port}/api/transactions/?${encodeURIComponent("filter[amount][<]")}=0`
+        const response = await axios.get(url);
+        expect(response.data?.resultSummary?.count).toBe(1)
+    });
     
-    let basicFilter = {
-        "amount": {
-            "<": "5"
-        }
-    }
-
-    beforeEach(() => {
-    });
-
-    test('test constructor', () => {
-        tq = new TransactionQueryFilter(basicFilter);
-
-        console.log("query:", tq.where)
-        console.log("tq:", tq)
-
-        // tq._addSqlConditionField(`%% ${''} LIKE ?`, ["tags", "manual_tags"], [`startsWif%`])
-
-        expect(clean(tq.where)).toBe(clean(' AND  (amount < CAST(? AS NUMERIC))'));
-        expect(tq.params[0]).toBe("5");
-
-    });
-})
+});
