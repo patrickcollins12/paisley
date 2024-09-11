57cbab91353e04a46f385891a0996581ef8913e6
Patrick Collins <pcollins1@gmail.com>
2024-09-11 10:20:42 +1000

    cleanup and commentary on FileWatcher strategy.

--- backend/src/FileWatcher.js
+++ backend/src/FileWatcher.js
@@ -1,7 +1,18 @@
 const chokidar = require('chokidar');
-const path = require('path');
-const fs = require('fs').promises;
 const Queue = require('better-queue');
+
+// chokidar is a file watcher and better-queue is a very simple in memory queue manager.
+//
+// Strategy:
+// We monitor for CSV files appearing and put them in a queue for processing.
+// when the queue has been empty for 1 second, we call the postProcessingFunc
+// 
+// The predominant post processing activity is the "classification" exercise.
+// The reason for this queuing and waiting is so that we can run the classifier 
+// over all rules (r) ONCE for each new transaction file (r) after all of the file
+//  processing has complete... this means the classification 
+// process is O(r) instead of O(r*t) which matters a lot when you're 
+// loading 500 CSV files.
 
 class FileWatcher {
   constructor(callfunc, postProcessingFunc) {
