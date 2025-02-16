aae2f6f2c8d5e390485b8df2eda62afa8af7dcf7
patrick <pcollins1@gmail.com>
2025-02-16 20:05:21 +1100

    jwt token.

--- backend/demo/demo_config.js
+++ backend/demo/demo_config.js
@@ -10,6 +10,25 @@ module.exports = {
     database:      path.join(paisleydir,   "demo/demo_transactions.db"),
     rules:         path.join(paisleydir,   "demo/demo_rules.js"),
     users_file:    path.join(paisleydir,   "users.json"),
+    log_directory: path.join(paisleydir, "logs"),
+    log_level: "info",
+
+    
+    // at what times should we scrape the accounts?
+    enable_scraper: true,
+    scrape_at_startup: false,
+    scheduled_scrape_cron: "01 * * * *", // on the 5th minute of every hour. See node-cron
+    scheduled_scrape_command: "npx playwright test --reporter json --retries 2",
+
+    // This is the key used to sign the JWT tokens. It should be a random string.
+    // remake the keys:
+    // e.g 58c8edc963952d08d14fe0fd622897615ef48c94bbdbd5c692996e9e24d7153a
+    // try using: openssl rand -hex 32
+    // generateRandom32CharHexString() {  // 32 bytes = 256 bits
+    //     return require('crypto').randomBytes(32).toString('hex');
+    // },
+    jwt: "XXXX",
+
 
     TestdataCSVParser: {
         identifier: "generic_test_data",
