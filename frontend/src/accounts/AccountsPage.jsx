ee877d18680f8618acccaadc189ed898e80f64c6
patrick <pcollins1@gmail.com>
2025-03-15 16:41:59 +1100

    almost working

--- frontend/src/accounts/AccountsPage.jsx
+++ frontend/src/accounts/AccountsPage.jsx
@@ -32,6 +32,15 @@ const AccountsPage = () => {
   const { t, i18n } = useTranslation();
 
 
+  const [expandedRows, setExpandedRows] = useState({});
+  const toggleExpand = (accountid) => {
+    setExpandedRows((prev) => ({
+      ...prev,
+      [accountid]: !prev[accountid], // Toggle expansion
+    }));
+  };
+
+
   let logoObject = null;
   if (data) {
     logoObject = logos[data.institution]
@@ -109,70 +118,97 @@ const AccountsPage = () => {
   }, [data]);
 
   function renderAccountDetails(category, totalValue, what_i_own_or_owe, total_assets_or_liabilities) {
-    return <>
-
-      {/* Assets Title */}
-      <TableRow className="bg-gray-100 dark:bg-gray-900 hover:bg-gray-200 dark:hover:bg-gray-800">
-        <TableCell className="md:table-cell p-2 font-bold" colSpan="7">
-          {t(what_i_own_or_owe)}
-        </TableCell>
-      </TableRow>
-
-
-      {accounts && accounts.filter((acc) => acc.category === category).map((account, index) => (
-
-        <TableRow
-          key={index}
-          className="border-t bg-opacity-90 transition duration-150 cursor-pointer"
-          onClick={() => navigate({ to: `/account/${account.accountid}` })}
-        >
-
-          {/* Icon and Short Name */}
-          <TableCell className="p-1 font-medium hover:underline  ">
-            <div className="flex items-center gap-3">
-              {logos && logos[account.institution] && logos[account.institution]['location'] &&
-                <span className={`ml-3 p-1 border ${logos[account.institution]['background']} rounded-lg`}>
-                  <img className="h-5" src={`${logos[account.institution]['location']}`} />
-                </span>}
-              <span>{account.shortname}</span>
-            </div>
+    
+    return (
+      <>
+        {/* Assets Title */}
+        <TableRow className="bg-gray-100 dark:bg-gray-900 hover:bg-gray-200 dark:hover:bg-gray-800">
+          <TableCell className="md:table-cell p-2 font-bold" colSpan="7">
+            {t(what_i_own_or_owe)}
           </TableCell>
-
-          {/* Account Type */}
-          <TableCell className="p-1">{account.type}</TableCell>
-
-          {/* Account Balance */}
-          <TableCell className="p-1 text-right">
-            {formatCurrency(account.balance, { currency: account.currency })}
-          </TableCell>
-
-          {/* Last Updated */}
-          <TableCell className={`p-1 hidden sm:table-cell text-xxs opacity-50 ${isStale(account.balance_datetime) ? 'text-red-500' : ''}`}>
-            <DateTimeDisplay datetime={account.balance_datetime} options={{ delta: true, absolute: false }} />
-          </TableCell>
-
-          {/* Sparkline */}
-          <TableCell className="p-0 hidden md:table-cell text-right">
-            {account && account.accountid &&
-              <AccountSparkLine accountid={account.accountid} />
-            }
-          </TableCell>
-
-          {/* Interest */}
-          <TableCell className="p-1 hidden md:table-cell text-right">{formatInterest(account.interest)}</TableCell>
-
         </TableRow>
-      ))}
 
-      {/* Total Assets Rows */}
-      <TableRow className="bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700 font-bold">
-        <TableCell colSpan="2" className="p-2">{t(total_assets_or_liabilities)}</TableCell>
-        <TableCell className="p-1 text-right">{formatCurrency(totalValue, { currency: "AUD" })}</TableCell>
-        <TableCell colSpan="1" className="p-1 font-bold hidden sm:table-cell"></TableCell>
-        <TableCell colSpan="3" className="p-1 font-bold hidden md:table-cell"></TableCell>
-      </TableRow>
+        {accounts &&
+          accounts
+            .filter((acc) => acc.category === category)
+            .map((account, index) => (
+              <React.Fragment key={account.accountid}>{renderRow(account)}</React.Fragment>
+            ))}
 
-    </>
+        {/* Total Assets Rows */}
+        <TableRow className="bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700 font-bold">
+          <TableCell colSpan="2" className="p-2">{t(total_assets_or_liabilities)}</TableCell>
+          <TableCell className="p-1 text-right">{formatCurrency(totalValue, { currency: "AUD" })}</TableCell>
+          <TableCell colSpan="1" className="p-1 font-bold hidden sm:table-cell"></TableCell>
+          <TableCell colSpan="3" className="p-1 font-bold hidden md:table-cell"></TableCell>
+        </TableRow>
+      </>
+    );
+
+    function renderRow(account) {
+      const isParent = account.parentid === null;
+      const isExpanded = expandedRows[account.accountid];
+
+      return (
+        <>
+          {/* Parent Row */}
+          <TableRow
+            key={account.accountid}
+            className="border-t bg-opacity-90 transition duration-150 cursor-pointer"
+          >
+            {/* Expand/Collapse Icon (Only for parent accounts) */}
+            <TableCell className="p-1 font-medium hover:underline">
+              <div className="flex items-center gap-3">
+                {isParent && (
+                  <button
+                    className="mr-1"
+                    onClick={() => toggleExpand(account.accountid)}
+                  >
+                    {isExpanded ? "▼" : "▶"}
+                  </button>
+                )}
+
+                {logos && logos[account.institution] && logos[account.institution]['location'] && (
+                  <span className={`ml-3 p-1 border ${logos[account.institution]['background']} rounded-lg`}>
+                    <img className="h-5" src={`${logos[account.institution]['location']}`} />
+                  </span>
+                )}
+                <span>{account.shortname}</span>
+              </div>
+            </TableCell>
+
+            {/* Account Type */}
+            <TableCell className="p-1">{account.type}</TableCell>
+
+            {/* Account Balance */}
+            <TableCell className="p-1 text-right">
+              {formatCurrency(account.balance, { currency: account.currency })}
+            </TableCell>
+
+            {/* Last Updated */}
+            <TableCell className={`p-1 hidden sm:table-cell text-xxs opacity-50 ${isStale(account.balance_datetime) ? "text-red-500" : ""}`}>
+              <DateTimeDisplay datetime={account.balance_datetime} options={{ delta: true, absolute: false }} />
+            </TableCell>
+
+            {/* Sparkline */}
+            <TableCell className="p-0 hidden md:table-cell text-right">
+              {account && account.accountid && <AccountSparkLine accountid={account.accountid} />}
+            </TableCell>
+
+            {/* Interest */}
+            <TableCell className="p-1 hidden md:table-cell text-right">{formatInterest(account.interest)}</TableCell>
+          </TableRow>
+
+          {/* Render Child Rows if Expanded */}
+          {isExpanded &&
+            accounts
+              .filter((acc) => acc.parentid === account.accountid)
+              .map((subAccount) => (
+                <React.Fragment key={subAccount.accountid}>{renderRow(subAccount)}</React.Fragment>
+              ))}
+        </>
+      );
+    }
   }
 
   return (
@@ -180,16 +216,16 @@ const AccountsPage = () => {
 
 
       <div className="flex flex-row mb-4">
-            <div className="flex flex-row basis-1/2 space-x-2">
-              <Button variant="outline" size="sm" className="h-8" asChild>
-                <Link to="/rules/new">
-                  <PlusIcon size={16} className="mr-1" />
-                  Create Account
-                </Link>
-              </Button>
-              <GlobalFilter dataTable={table} />
-            </div>
-          </div>
+        <div className="flex flex-row basis-1/2 space-x-2">
+          <Button variant="outline" size="sm" className="h-8" asChild>
+            <Link to="/rules/new">
+              <PlusIcon size={16} className="mr-1" />
+              Create Account
+            </Link>
+          </Button>
+          <GlobalFilter dataTable={table} />
+        </div>
+      </div>
 
 
       <div className="flex flex-col items-center mb-4">
@@ -232,7 +268,7 @@ const AccountsPage = () => {
                   <>
                     {/* If loaded, let's display the table! */}
 
-                    {renderAccountDetails(    "asset",      totalAssets,      "What I own (Assets)",      "Total (Assets)")}
+                    {renderAccountDetails("asset", totalAssets, "What I own (Assets)", "Total (Assets)")}
                     {renderAccountDetails("liability", totalLiabilities, "What I owe (Liabilities)", "Total (Liabilities)")}
 
                     {/* Net worth */}
