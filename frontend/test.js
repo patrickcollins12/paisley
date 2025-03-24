0325b0b24bf4f29d2c1183a5b12b2483a60d9277
patrick <pcollins1@gmail.com>
2025-03-24 17:40:41 +1100

    added visualizer for stats

--- frontend/test.js
+++
@@ -1,7 +0,0 @@
-import { DateTime } from 'luxon';
-
-
-// Get list of all available timezones
-const timezones = DateTime.local().setZone('UTC').zoneName;
-
-console.log(timezones);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,19 @@ import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 // import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import { resolve } from "path"
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
-    react()
+    react(),
+    visualizer({
+      filename: './dist/stats.html', // output location
+      open: true,                    // auto-open after build
+      gzipSize: true,
+      brotliSize: true
+    }),
   ],
 
   resolve: {
@@ -18,7 +25,7 @@ export default defineConfig({
   },
 
   server: {
-    // host: '0.0.0.0', // Allow connections from all IPs
+    host: '0.0.0.0', // Allow connections from all IPs
     strictPort: true, // Optional: prevent the server from picking a random port if the specified one is taken
     proxy: {
       '/api': {
